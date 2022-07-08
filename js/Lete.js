dc3ea038c7e30373d57c6a5132375078b7f88038
WeiWu-code <3517119781@qq.com>
2022-07-08 09:22:07 +0800

    Site updated: 2022-07-08 09:22:07

---
+++ js/Lete.js
@@ -0,0 +1,4 @@
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2022 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> WeiWu');
+})
